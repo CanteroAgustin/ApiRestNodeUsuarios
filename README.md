75add4717ac84c94adfd57c21690b69bdbcf962b
Agustin Cantero <agustincantero11@gmail.com>
2019-09-13 23:04:40 -0300

    get & post usuarios

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 "# ApiRestNodeUsuarios" 
+
+ObtenerUsuarios:
+GET http://localhost:3000/users
+
+Insertar Usuario
+POST http://localhost:3000/users
+header: "Content-Type" "application/x-www-form-urlencoded"
+body: {"key":"name","value":[name],"description":"","type":"text","enabled":true},{"key":"apellido","value":"[apellido]","description":"","type":"text","enabled":true},{"key":"userName","value":"[userName]","description":"","type":"text","enabled":true},{"key":"password","value":"[password]","description":"","type":"text","enabled":true},{"key":"rol","value":"[rol]","description":"","type":"text","enabled":true},{"key":"documento","value":"[documento]","description":"","type":"text","enabled":true},{"key":"foto","value":"\"\"","description":"","type":"text","enabled":true}
---
+++ routes/users.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var router = express.Router();
+var mysql = require('mysql');
+
+
+var con = mysql.createConnection({
+  host: "sql124.main-hosting.eu",
+  user: "u933428110_agus",
+  password: "SANiSZULC8cs",
+  database: "u933428110_apies"
+});
+
+/* GET users listing. */
+router.get('/', function (req, res, next) {
+  var sql = "SELECT * from usuarios";
+  
+    con.query(sql, function (err, result) {
+      if (err){
+        console.log(err);
+      } 
+      console.log("Result: " + JSON.stringify(result));
+      res.send(result);
+    });
+});
+
+/* GET users listing. */
+router.post('/', function (req, res, next) {
+  var body = {nombre:req.body.name,apellido:req.body.apellido,userName:req.body.userName,password:req.body.password,
+    rol:req.body.rol,documento:req.body.documento,foto:req.body.foto}
+  
+    con.query('INSERT INTO usuarios SET ?',body, function (err, result) {
+      if (err){
+        console.log(err);
+      } 
+      console.log("Result: " + JSON.stringify(result));
+      res.send(result);
+    });
+});
+
+module.exports = router;
