6ca7b95f61a6e2afcb5a20003ba2108e6325ddd0
Agustin Cantero <agustincantero11@gmail.com>
2019-09-15 22:37:30 -0300

    otro

--- routes/users.js
+++ routes/users.js
@@ -10,27 +10,26 @@ var con = mysql.createConnection({
   database: "u933428110_apies"
 });
 
-var disconnect = function() {
-  con.on('error', function(err) {
-      if (!err.fatal) {
-          return;
-      }
-      if (err.code !== 'PROTOCOL_CONNECTION_LOST') {
-          console.log("PROTOCOL_CONNECTION_LOST");
-          throw err;
-      }
-      log.error("The database is error:" + err.stack);
+function handleDisconnect() {
+  connection = mysql.createConnection(db_config); // Recreate the connection, since
+                                                  // the old one cannot be reused.
 
-      kfdb = mysql.createConnection({
-        host: "sql124.main-hosting.eu",
-        user: "u933428110_agus",
-        password: "SANiSZULC8cs",
-        database: "u933428110_apies"
-      });
-      disconnect();
+  connection.connect(function(err) {              // The server is either down
+    if(err) {                                     // or restarting (takes a while sometimes).
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+    }                                     // to avoid a hot loop, and to allow our node script to
+  });                                     // process asynchronous requests in the meantime.
+                                          // If you're also serving http, display a 503 error.
+  connection.on('error', function(err) {
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+      handleDisconnect();                         // lost due to either server restart, or a
+    } else {                                      // connnection idle timeout (the wait_timeout
+      throw err;                                  // server variable configures this)
+    }
   });
- };
- disconnect();
+}
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
