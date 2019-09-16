73aff79f6facc653dc3a64623439c9269d1e38a7
Agustin Cantero <agustincantero11@gmail.com>
2019-09-15 22:31:52 -0300

    otro

--- routes/users.js
+++ routes/users.js
@@ -10,35 +10,55 @@ var con = mysql.createConnection({
   database: "u933428110_apies"
 });
 
-con.on('error', function (err) {
-  console.log('error en base de datos: ' + err.toString());
-});
+var disconnect = function() {
+  con.on('error', function(err) {
+      if (!err.fatal) {
+          return;
+      }
+      if (err.code !== 'PROTOCOL_CONNECTION_LOST') {
+          console.log("PROTOCOL_CONNECTION_LOST");
+          throw err;
+      }
+      log.error("The database is error:" + err.stack);
+
+      kfdb = mysql.createConnection({
+        host: "sql124.main-hosting.eu",
+        user: "u933428110_agus",
+        password: "SANiSZULC8cs",
+        database: "u933428110_apies"
+      });
+      disconnect();
+  });
+ };
+ handleKFDisconnect();
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
   var sql = "SELECT * from usuarios";
-  
-    con.query(sql, function (err, result) {
-      if (err){
-        next(err);
-      } 
-      console.log("Result: " + JSON.stringify(result));
-      res.send(result);
-    });
+
+  con.query(sql, function (err, result) {
+    if (err) {
+      next(err);
+    }
+    console.log("Result: " + JSON.stringify(result));
+    res.send(result);
+  });
 });
 
 /* GET users listing. */
 router.post('/', function (req, res, next) {
-  var body = {nombre:req.body.nombre,apellido:req.body.apellido,userName:req.body.userName,password:req.body.password,
-    rol:req.body.rol,documento:req.body.documento,foto:req.body.foto}
-  
-    con.query('INSERT INTO usuarios SET ?',body, function (err, result) {
-      if (err){
-        next(err);
-      } 
-      console.log("Result: " + JSON.stringify(result));
-      res.send(result);
-    });
+  var body = {
+    nombre: req.body.nombre, apellido: req.body.apellido, userName: req.body.userName, password: req.body.password,
+    rol: req.body.rol, documento: req.body.documento, foto: req.body.foto
+  }
+
+  con.query('INSERT INTO usuarios SET ?', body, function (err, result) {
+    if (err) {
+      next(err);
+    }
+    console.log("Result: " + JSON.stringify(result));
+    res.send(result);
+  });
 });
 
 module.exports = router;
