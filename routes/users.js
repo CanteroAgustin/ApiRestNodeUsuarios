b0fc2209d0e187bc79f085a6f7fc76c335ea905f
Agustin Cantero <agustincantero11@gmail.com>
2019-09-15 21:27:18 -0300

    otro

--- routes/users.js
+++ routes/users.js
@@ -13,30 +13,34 @@ var con = mysql.createConnection({
 /* GET users listing. */
 router.get('/', function (req, res, next) {
   var sql = "SELECT * from usuarios";
-  
-    con.query(sql, function (err, result) {
-      if (err){
-        console.log(err);
-      } 
-      console.log("Result: " + JSON.stringify(result));
-      res.send(result);
-      res.end();
-    });
+
+  con.query(sql, function (err, result) {
+    if (err) {
+      console.log(err);
+    }
+    console.log("Result: " + JSON.stringify(result));
+    res.send(result);
+    res.end();
+  });
+  req.abort();
 });
 
 /* GET users listing. */
 router.post('/', function (req, res, next) {
-  var body = {nombre:req.body.nombre,apellido:req.body.apellido,userName:req.body.userName,password:req.body.password,
-    rol:req.body.rol,documento:req.body.documento,foto:req.body.foto}
-  
-    con.query('INSERT INTO usuarios SET ?',body, function (err, result) {
-      if (err){
-        console.log(err);
-      } 
-      console.log("Result: " + JSON.stringify(result));
-      res.send(result);
-      res.end();
-    });
+  var body = {
+    nombre: req.body.nombre, apellido: req.body.apellido, userName: req.body.userName, password: req.body.password,
+    rol: req.body.rol, documento: req.body.documento, foto: req.body.foto
+  }
+
+  con.query('INSERT INTO usuarios SET ?', body, function (err, result) {
+    if (err) {
+      console.log(err);
+    }
+    console.log("Result: " + JSON.stringify(result));
+    res.send(result);
+    res.end();
+  });
+  req.abort();
 });
 
 module.exports = router;
