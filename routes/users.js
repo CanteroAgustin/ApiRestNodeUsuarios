48d2de0d0cd89bcea37a8e9c9f0049cb7e9f6c20
Agustin Cantero <agustincantero11@gmail.com>
2019-09-15 22:45:07 -0300

    otro

--- routes/users.js
+++ routes/users.js
@@ -10,35 +10,9 @@ var con = mysql.createConnection({
   database: "u933428110_apies"
 });
 
-var connection;
-
-function handleDisconnect() {
-  connection = con.createConnection({
-    host: "sql124.main-hosting.eu",
-    user: "u933428110_agus",
-    password: "SANiSZULC8cs",
-    database: "u933428110_apies"
-  }); // Recreate the connection, since
-                                                  // the old one cannot be reused.
-
-  connection.connect(function(err) {              // The server is either down
-    if(err) {                                     // or restarting (takes a while sometimes).
-      console.log('error when connecting to db:', err);
-      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
-    }                                     // to avoid a hot loop, and to allow our node script to
-  });                                     // process asynchronous requests in the meantime.
-                                          // If you're also serving http, display a 503 error.
-  connection.on('error', function(err) {
-    console.log('db error', err);
-    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
-      handleDisconnect();                         // lost due to either server restart, or a
-    } else {                                      // connnection idle timeout (the wait_timeout
-      throw err;                                  // server variable configures this)
-    }
-  });
-}
-
-handleDisconnect();
+setInterval(function () {
+  con.query('SELECT 1');
+}, 5000);
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
